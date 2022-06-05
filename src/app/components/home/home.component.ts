40067a5e91e47a55b30f1fae532f6d405907a74c
Fernando Sandoval <csandoval.1012448209>
2022-06-04 20:31:51 -0500

    visual

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, NgZone, OnInit } from "@angular/core";
 import * as moment from "moment";
 import { BitcoinServiceService } from "src/app/services/bitcoin-service.service";
 
@@ -8,9 +8,12 @@ import { BitcoinServiceService } from "src/app/services/bitcoin-service.service"
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  constructor(private bitcoinService: BitcoinServiceService) {}
+  constructor(
+    private bitcoinService: BitcoinServiceService,
+    public ngzone: NgZone
+  ) {}
 
-  public btc = 0;
+  public btc:any;
   public detail_btc = [];
   ngOnInit() {
     this.getBitcoin();
@@ -26,7 +29,7 @@ export class HomeComponent implements OnInit {
         .getBitcoin("bitcoin", "usd")
         .then((res: any) => {
           console.log(res);
-          this.btc = res.bitcoin.usd;
+          this.btc = Intl.NumberFormat().format(res.bitcoin.usd);
         });
     } catch (error) {}
   }
@@ -40,13 +43,20 @@ export class HomeComponent implements OnInit {
           for (let i = 0; i < res.prices.length; i++) {
             const prices = res.prices[i];
             this.detail_btc.push({
+              date_unix: prices[0],
               date: moment(moment.unix(prices[0]).unix()).format(
                 "DD MMM YYYY hh:mm a"
               ),
               currenc: "USD",
-              price: prices[1],
+              price: Intl.NumberFormat().format(Math.round(prices[1])),
             });
           }
+          this.ngzone.run(()=>{
+            this.detail_btc.sort(
+              (a, b) => parseInt(a.date_unix) - parseInt(b.date_unix)
+            );
+          })
+          
           console.log("detalle", this.detail_btc);
         });
     } catch (error) {}
